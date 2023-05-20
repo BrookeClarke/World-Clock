435619ccc99f01b6adc6faa285ac08f3fd9b3e09
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-05-20 20:47:46 +0100

    Added some more places onto it😊☺️

--- app.js
+++ app.js
@@ -17,8 +17,38 @@ function updateTime() {
         ParisDateElement.innerHTML = ParisTime.format("dddd Do MMMM YYYY");
         ParisTimeElement.innerHTML = ParisTime.format("HH:mm:ss[<small>] A [</small>]");
     }
+    // Madrid Date and Time
+    let SpainElement = document.querySelector("#Spain");
+    if (SpainElement) {
+        SpainDateElement = SpainElement.querySelector(".date");
+        SpainTimeElement = SpainElement.querySelector(".time");
+        let SpainTime = moment().tz("Europe/Madrid");
+        SpainDateElement.innerHTML = SpainTime.format("dddd Do MMMM YYYY");
+        SpainTimeElement.innerHTML = SpainTime.format("HH:mm:ss[<small>] A [</small>]");
+    }
+    // Cairo Date and Time
+    let CairoElement = document.querySelector("#Cairo");
+    if (CairoElement) {
+        CairoDateElement = CairoElement.querySelector(".date");
+        CairoTimeElement = CairoElement.querySelector(".time");
+        let CairoTime = moment().tz("Africa/Cairo");
+        CairoDateElement.innerHTML = CairoTime.format("dddd Do MMMM YYYY");
+        CairoTimeElement.innerHTML = CairoTime.format("HH:mm:ss[<small>] A [</small>]");
+    }
+    // New York Date and Time
+    let NewYorkElement = document.querySelector("#New_York");
+    if (NewYorkElement) {
+        NewYorkDateElement = NewYorkElement.querySelector(".date");
+        NewYorkTimeElement = NewYorkElement.querySelector(".time");
+        let NewYorkTime = moment().tz("America/New_York");
+        NewYorkDateElement.innerHTML = NewYorkTime.format("dddd Do MMMM YYYY");
+        NewYorkTimeElement.innerHTML = NewYorkTime.format("HH:mm:ss[<small>] A [</small>]");
+    }
 }
-    
+
+
+
+    // Cities change when the select is used
     function updateCity(event) {
         let cityTimeZone = event.target.value;
         if (cityTimeZone === "current") {
